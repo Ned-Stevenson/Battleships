b8e81b51f980f3e41f9924873828668c52d8ae41
stuntphish <stuntphish@gmail.com>
2020-11-22 11:43:01 +0000

    Started creating a GUI and menu system with Tkinter

--- Ui.py
+++ Ui.py
@@ -1,13 +1,162 @@
 from game import Game, SHIPS, vertical, horizontal
 from exceptions import ShotError
+from tkinter import Tk, Frame, Button, Grid, Label, Entry, N, S, E, W, X, Y, BOTH, BOTTOM
+from itertools import product
 
 class Ui:
     def __init__(self):
         pass
 
-class Gui(Ui):
+class Gui(Ui): #To Do
     def __init__(self):
+        self._defaultText = ("Consolas", 20)
+        self._background = "navy"
+        self._text = "white"
+        self._defaultLayout = {"bg":self._background, "fg": self._text, "font":("Consolas", 20)}
+        #Preparing the window and the main menu frame
+        root = Tk()
+        root.overrideredirect(True) # Makes the application borderless
+        root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0")
+        root.title("Battleships")
+        frame = Frame(root)
+        frame.pack(fill = BOTH, expand = True)
+        # These buttons will be displayed on the main menu
+        Button(frame, text = "Play Game", command = self.__playGameMenu, **self._defaultLayout).grid(row = 0, column = 0, sticky = N + S + E + W)
+        Button(frame, text = "Settings", command = self.__settings, **self._defaultLayout).grid(row = 1, column = 0, sticky = N + S + E + W)
+        Button(frame, text = "User stats", command = self.__userStats, **self._defaultLayout).grid(row = 2, column = 0, sticky = N + S + E + W)
+        Button(frame, text = "Exit", command = root.quit, **self._defaultLayout).grid(row = 3, column = 0, sticky = N + S + E + W)
+
+        # Weighting the rows and columns to make all of the buttons on the screen of equal size
+        frame.grid_columnconfigure(0, weight = 1)
+        for i in range(4):
+            frame.grid_rowconfigure(i, weight = 1)
+
+        self._root = root
+        self._mainMenu = frame
+        self._currentFrame = frame
+    
+    def run(self):
+        self._root.mainloop()
+    
+    def __playGameMenu(self):
+        # Unrender the previous frame and replace it with the game menu frame
+        self._mainMenu.pack_forget()
+        frame = Frame(self._root, bg = self._background)
+        frame.pack(fill = BOTH, expand = True)
+        self._currentFrame = frame
+
+        # Adding widgets to the frame using the grid placement
+        player1Box = Entry(frame, font = self._defaultText)
+        player1Box.insert(0, "Player 1")
+        player2Box = Entry(frame, font = self._defaultText)
+        player2Box.insert(0, "Player 2")
+        player1Box.grid(row = 0, column = 0)
+        player2Box.grid(row = 1, column = 0)
+        # This lambda, when called 
+        startGame = lambda: self.__startGame(player1Box.get(), player2Box.get())
+        Button(frame, text = "Start game", command = startGame, **self._defaultLayout).grid(row = 0, column = 1, rowspan = 2, sticky = N+S+E+W)
+        Button(frame, text = "Return to main menu", command = self.__returnToMainMenu, **self._defaultLayout).grid(row = 2, column = 0, columnspan = 2, sticky = N+S+E+W)
+
+        for i in range(3):
+            frame.grid_rowconfigure(i, weight = 1)
+        frame.grid_columnconfigure(0, weight = 2)
+        frame.grid_columnconfigure(1, weight = 1)
+
+    def __startGame(self, player1Name, player2Name):
+        # game = Game(player1Name, player2Name)
+        # Creating a frame for the whole window and placing a board frame within the larger frame
+        self._currentFrame.pack_forget()
+        frame = Frame(self._root, bg = self._background)
+        frame.pack(fill = BOTH, expand = True)
+        self._currentFrame = frame
+
+        board = Frame(frame, bg = self._background)
+        board.grid(row = 0, column = 0, sticky = N+S+E+W, rowspan = 3)
+        
+        frame.grid_rowconfigure(0, weight = 2)
+        frame.grid_columnconfigure(0, weight = 4)
+        frame.grid_columnconfigure(1, weight = 1)
+        frame.grid_columnconfigure(2, weight = 1)
+
+        # Creating the tiling of buttons as well as the row and column labels
+        for row, col in product(range(Game.dim+1), range(Game.dim+1)):
+            if row == 0:
+                if col == 0:
+                    # Ensuring nothing unexpected is placed in the top left corner
+                    continue
+                else:
+                    Label(board, text = chr(col+64), **self._defaultLayout).grid(row = row, column = col, sticky = N+S+E+W)
+            elif col == 0:
+                Label(board, text = str(row), **self._defaultLayout).grid(row = row, column = col, sticky = N+S+E+W)
+            else:
+                # To do Create a function or lambda to be called when the button is pressed
+                Button(board, **self._defaultLayout).grid(row = row, column = col, sticky = N+S+E+W)
+        
+        # Ensuring all the tiles in the grid are square
+        for row in range(Game.dim+1):
+            board.grid_rowconfigure(row, weight = 1)
+        for col in range(Game.dim+1):
+            board.grid_columnconfigure(col, weight = 1)
+        
+        # The console will be a list of events like a computer console that will remind the player what has happened
+        # It will be a frame with Labels packed onto the frame after each event
+        console = Frame(frame, bg = "black")
+        console.grid(row = 0, column = 1, sticky = N+S+E+W)
+        
+        # To Do Implement adding messages to the console when an event occurs
+
+        shipIcons = Frame(frame, bg = self._background)
+        shipIcons.grid(row = 0, column = 2, sticky = N+S+E+W)
+
+        Button(frame, **self._defaultLayout, command = self.__returnToMainMenu, text = "Return to main menu").grid(row = 1, column = 1, sticky = N+S+E+W, columnspan = 2)
+        Button(frame, **self._defaultLayout, text = "Save Game").grid(row = 2, column = 1, sticky = N+S+E+W, columnspan = 2)
+
+    def __settings(self): #To Do Change text colour and button colour
+        # This removes the main menu from the root window
+        self._mainMenu.pack_forget()
+        # Preparing and adding the frame to the screen
+        message = "Sorry, settings are not yet available"
+        frame = Frame(self._root)
+        frame.pack(fill = BOTH, expand = True)
+        Label(frame, text = message, **self._defaultLayout).grid(row = 0, column = 0, sticky = N+S+E+W)
+        Button(frame, text = "Return to main menu", command = self.__returnToMainMenu, **self._defaultLayout).grid(row = 1, column = 0, sticky = N+S+E+W)
+        # This means that the row of the grid containing the text will always be 3 times larger than the row for the button
+        frame.grid_rowconfigure(0, weight = 3)
+        frame.grid_rowconfigure(1, weight = 1)
+        frame.grid_columnconfigure(0, weight = 1)
+        self._currentFrame = frame
+
+    def __userStats(self): #To Do Change text colour and button colour
+        # This removes the main menu from the root window
+        self._mainMenu.pack_forget()
+        # Preparing and adding the frame to the screen
+        message = "Sorry, user stats are not yet available"
+        frame = Frame(self._root)
+        frame.pack(fill = BOTH, expand = True)
+        Label(frame, text = message, **self._defaultLayout).grid(row = 0, column = 0, sticky = N+S+E+W)
+        Button(frame, text = "Return to main menu", command = self.__returnToMainMenu, **self._defaultLayout).grid(row = 1, column = 0, sticky = N+S+E+W)
+        # This means that the row of the grid containing the text will always be 3 times larger than the row for the button
+        frame.grid_rowconfigure(0, weight = 3)
+        frame.grid_rowconfigure(1, weight = 1)
+        frame.grid_columnconfigure(0, weight = 1)
+        self._currentFrame = frame
+    
+    def __returnToMainMenu(self):
+        self._currentFrame.pack_forget()
+        self._mainMenu.pack(fill = BOTH, expand = True)
+
+class ConsoleWidget(Frame):
+    def __init__(self, root, background = "green", foreground = "black", font = ("Courier New", 12)):
+        self.__numberOfConsoleItems = 0
+        self.__backgroundColour = background
+        self.__textColour = foreground
+        self.__font = font
+        self.__frame = Frame(root, bg = background)
         raise NotImplementedError
+
+    def addConsoleItem(self, message):
+        Label(self.__frame, fg = self.__textColour, bg = self.__backgroundColour, font = self.__font, text = message, anchor = W).grid(row = self.__numberOfConsoleItems, column = 0, sticky = N+S+E+W)
+        self.__numberOfConsoleItems += 1
 
 class Terminal(Ui):
     def __init__(self):
--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,7 @@
 class ShotError(Exception):
     def __init__(self):
         pass
+
+class ShipPlacementError(Exception):
+    def __init__(self):
+        pass
